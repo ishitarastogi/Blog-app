cc1aa9b8019d8f04c7b6430b1b58b929bacd20b2
ishitarastogi <ishita3099@gmail.com>
2022-01-17 12:06:32 +0530

    Add routes and models

--- index.js
+++ index.js
@@ -4,9 +4,35 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL,()=>{
+    console.log('connect to dbs')
+});
+ 
 
-app.listen(8800,()=>{
+//middleware
+//parse incoming request 
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+ app.use('/api/users',userRoute)
+app.use('/api/auth',authRoute)
+
+// app.get('/users',(req, res, next)=>{
+//     res.send("welcome to users page")
+// })
+
+// app.get('/',(req, res, next)=>{
+//     res.send("welcome to home page")
+// })
+
+
+
+
+app.listen(3008,()=>{
     console.log('listening on port 8800')
 });
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose= require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    userName:{
+        type: String,
+        require: true,
+        min:3,
+        max:20,
+        unique: true
+    },
+    email:{
+        type: String,
+        require: true,
+        max:50,
+        unique: true
+    },
+    password:{
+        type: String,
+        require: true,
+        min:6
+    },
+    profilePicture:{
+        type: String,
+        default:"" 
+    },
+    coverPicture: {
+        type: String,
+        default:""
+    },
+    following:{
+        type: Array,
+        default:[]
+    },
+    followers:{
+        //we keep user id inside this array'
+        type: Array,
+        default:[]
+    },
+    isAdmin:{
+       type: Boolean,
+       default:false
+    }
+
+});
